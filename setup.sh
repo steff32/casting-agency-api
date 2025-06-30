fdd6d81c89ecbe72c0391ac219adb94307cb30c7
Stefani Genkova <stafeni.genkova@bluebank.io>
2025-07-01 02:22:37 +0300

    Update db url

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-# export DATABASE_URL='postgresql://casting_agency_fmcl_user:LGlVAzG3yinuLvpfjjKp3zm1976m4VrR@dpg-d1hd0k7fte5s739kdn7g-a/casting_agency_fmcl'
-export DATABASE_URL='postgresql://stefani.genkova:@localhost:5432/casting_agency'
+export DATABASE_URL='postgresql://casting_agency_fmcl_user:LGlVAzG3yinuLvpfjjKp3zm1976m4VrR@dpg-d1hd0k7fte5s739kdn7g-a/casting_agency_fmcl'
+
 export AUTH0_DOMAIN='dev-tcmculz33ufggfvb.us.auth0.com'
 export API_AUDIENCE='agency'
 export ALGORITHMS=['RS256']
